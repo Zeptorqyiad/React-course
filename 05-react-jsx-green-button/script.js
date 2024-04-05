03bee636473c77ce5fe00fc7f4bb965245896059
Max Unanian <Zeptorqyiad99@gmail.com>
2024-04-06 01:19:34 +0500

    Add new props in scripts.js file

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,17 +1,21 @@
-const App = () => {
-    const [buttonText, setButtonText] = React.useState('Click me')
+const App = ({ intialButtonText }) => {
+   const [buttonText, setButtonText] = React.useState(intialButtonText)
+   const [classesList, setClassesList] = React.useState('')
 
-    const onButtonClick = () => {
-        setButtonText('Hello from React')
-    }
-	 
-    return (
-        <div className="app">
-            <button onClick={onButtonClick}>{buttonText}</button>
-        </div>
-    )
+   const onButtonClick = () => {
+      setButtonText('Hello from React')
+      setClassesList('green-btn')
+   }
+
+   return (
+      <div className="app">
+         <button className={classesList} onClick={onButtonClick}>
+            {buttonText}
+         </button>
+      </div>
+   )
 }
 
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
-root.render(<App />)
+root.render(<App intialButtonText="Click me" />)
