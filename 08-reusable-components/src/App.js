32c3bfe7576b904c1763373e1feb3506af8f7eb0
Max Unanian <Zeptorqyiad99@gmail.com>
2024-04-09 20:52:27 +0500

    Added new 08 file in course React

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import MyComponent from './components/MyComponent'
+
+function App() {
+   return (
+      <div className="App">
+         <MyComponent />
+         <MyComponent />
+         <MyComponent />
+      </div>
+	)
+}
+
+export default App
