fe7faf1b2013b7a7f756f8c56ae248a189ed4f6c
Max Unanian <Zeptorqyiad99@gmail.com>
2024-04-17 23:27:29 +0500

    Added loading in file

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -4,18 +4,29 @@ import Post from './Post'
 
 function Posts() {
    const [posts, setPosts] = useState([])
+   const [error, setError] = useState('')
+   const [isLoading, setIsLoading] = useState(true)
 
    useEffect(() => {
       fetch('https://jsonplaceholder.typicode.com/posts')
          .then((res) => res.json())
          .then((posts) => setPosts(posts))
-         .catch((error) => console.log(error.message))
+         .catch((error) => setError(error.message))
+         .finally(() => setIsLoading(false))
    }, [])
+
+   if (isLoading) {
+      return <h1>Loading...</h1>
+   }
+
+   if (error) {
+      return <h1>Error: {error}</h1>
+   }
 
    return (
       <div>
          {posts.map((post) => (
-            <Post {...post} key={post.id}/>
+            <Post {...post} key={post.id} />
          ))}
       </div>
    )
