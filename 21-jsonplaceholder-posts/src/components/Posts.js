4de5e5c4da714b4fd7b01368c09fb322da051e4e
Max Unanian <Zeptorqyiad99@gmail.com>
2024-04-17 23:16:16 +0500

    Added new 21-file in course React

---
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Post from './Post'
+
+function Posts() {
+   const [posts, setPosts] = useState([])
+
+   useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+         .then((res) => res.json())
+         .then((posts) => setPosts(posts))
+         .catch((error) => console.log(error.message))
+   }, [])
+
+   return (
+      <div>
+         {posts.map((post) => (
+            <Post {...post} key={post.id}/>
+         ))}
+      </div>
+   )
+}
+
+export default Posts
