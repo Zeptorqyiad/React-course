d19ec3aa20c6ecee87ce2388774710723dae9469
Max Unanian <Zeptorqyiad99@gmail.com>
2024-04-28 16:07:37 +0500

    Added new ksx file in 25-file

--- 25-todo-app-v2/src/App.jsx
+++ 25-todo-app-v2/src/App.jsx
@@ -1,24 +1,46 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import './App.css'
 import TodoForm from './components/Todos/TodoForm'
 import TodoList from './components/Todos/TodoList'
+import TodosActions from './components/Todos/TodosAction'
 
 function App() {
    const [todos, setTodos] = useState([])
 
    const addTodoHandler = (text) => {
-      setTodos([...todos, text])
+      const newTodo = {
+         text,
+         isCompleted: false,
+         id: uuidv4(),
+      }
+      setTodos([...todos, newTodo])
    }
 
-   const deleteTodoHandler = (index) => {
-      setTodos(todos.filter((_, idx) => idx !== index))
+   const deleteTodoHandler = (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id))
+   }
+
+   const toggleTodoHandler = (id) => {
+      setTodos(
+         todos.map((todo) => {
+            return todo.id === id
+               ? { ...todo, isCompleted: !todo.isCompleted }
+               : { ...todo }
+         })
+      )
    }
 
    return (
       <div className="App">
          <h1>Todo App</h1>
          <TodoForm addTodo={addTodoHandler} />
-         <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+			<TodosActions />
+         <TodoList
+            todos={todos}
+            deleteTodo={deleteTodoHandler}
+            toggleTodo={toggleTodoHandler}
+         />
       </div>
    )
 }
