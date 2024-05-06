8dad455755635f3d40e860721a64259a7178301e
Max Unanian <Zeptorqyiad99@gmail.com>
2024-05-06 16:43:27 +0500

    Finished 26-file in course React

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,27 +1,23 @@
 import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 import courses from '../data/courses'
-import NotFound from './NotFound'
 
 const SingleCourse = () => {
    const params = useParams()
-   useNavigate()
+   const navigate = useNavigate()
    const course = courses.find((course) => course.slug === params.courseSlug)
-   if (!course) {
-      return (
-         <>
-            <NotFound />
-            <Link to=".." relative="path">
-               All courses
-            </Link>
-         </>
-      )
-   }
+
+   useEffect(() => {
+      if (!course) {
+         navigate('..', { relative: 'path' })
+      }
+   }, [course, navigate])
 
    return (
       <>
-         <h1>{course.title}</h1>
-         <h2>{course.slug}</h2>
-         <h3>{course.id}</h3>
+         <h1>{course?.title}</h1>
+         <h2>{course?.slug}</h2>
+         <h3>{course?.id}</h3>
          <Link to=".." relative="path">
             All courses
          </Link>
